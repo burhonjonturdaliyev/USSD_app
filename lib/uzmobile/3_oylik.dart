5fa358e8d3e34201bba3e8e9dac89054b22df7b2
Burkhonjon Turdialiev <burhonjonturdaliyev2003@gmail.com>
2022-12-24 20:40:14 +0500

    USSD_codelari

---
+++ lib/uzmobile/3_oylik.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:ussd_kodlari/main.dart';
+
+class Super_month extends StatefulWidget {
+  const Super_month({super.key});
+
+  @override
+  State<Super_month> createState() => _Super_monthState();
+}
+
+class _Super_monthState extends State<Super_month> {
+  @override
+  Widget build(BuildContext context) {
+    return super_month_items();
+  }
+}
+
+Widget super_month_items() {
+  return Column(
+    children: [
+      Row(
+        children: [],
+      )
+    ],
+  );
+}
--- lib/uzmobile/units.dart
+++ lib/uzmobile/units.dart
@@ -10,7 +10,7 @@ class Units extends StatelessWidget {
   const Units({super.key});
   static List<Unit_packages> units = [
     Unit_packages(
-        package_name: "UNITS 700",
+        package_name: "700",
         monthly: "7 kunga 7000",
         limits: "700",
         contents:
@@ -19,14 +19,68 @@ class Units extends StatelessWidget {
             "Units tariff rejalarida oylik internet paketlar xarid qilib bo'lmaydi va berilgan Unitlar tugagandan so'ng hisobidagi mablag' orqali so'zlashib bo'lmaydi tarif reajasi bo'yicha abonent to'lovi to'lab restart (*555#) xizmatini faollashtirish mumkin!",
         codes: "*777*1#"),
     Unit_packages(
-        package_name: "UNITS 150",
+        package_name: "150",
         monthly: "15 kunga 15000",
         limits: "1500",
         contents:
             "1 Unit = 1mb, 1daqiqa, 1sms Yani berilgan Unitlarni extiyojga qarab istalgan shaklda sarflash mumkin. Misol berilgan 1500 Unitlarni 1500 mb uchun yoki 1500 daqiqa so'zlashish uchun foydalanish mumkin yoki 750 mb va 750 daqiqa uchun.",
         reminder:
             "Units tariff rejalarida oylik internet paketlar xarid qilib bo'lmaydi va berilgan Unitlar tugagandan so'ng hisobdagi mablag' orqali so'zlashib bo'lmaydi tarif rejasi tarif rejasi bo'yicha abonent to'lovi to'lab restar (*555#) xizmatini faollashtirish mumkin!",
-        codes: "*777*2#")
+        codes: "*777*2#"),
+    Unit_packages(
+        package_name: "4000",
+        monthly: "30 kunga 20000",
+        limits: "4000",
+        contents:
+            "1 Unit = 1mb, 1daqiqa, 1sms Yani berilgan Unitlarni extiyojga qarab istalgan shaklda sarflash mumkin. Misol berilgan 4000 Unitlarni 4000 mb uchun yoki 2000 daqiqa so'zlashish uchun foydalanish mumkin yoki 2000 mb va 2000 daqiqa uchun.",
+        reminder:
+            "Units tariff rejalarida oylik internet paketlar xarid qilib bo'lmaydi va berilgan Unitlar tugagandan so'ng hisobdagi mablag' orqali so'zlashib bo'lmaydi tarif rejasi tarif rejasi bo'yicha abonent to'lovi to'lab restar (*555#) xizmatini faollashtirish mumkin!",
+        codes: "*777*3#"),
+    Unit_packages(
+        package_name: "8000",
+        monthly: "Oyiga 35000",
+        limits: "8000",
+        contents:
+            "1 Unit = 1mb, 1daqiqa, 1sms Yani berilgan Unitlarni extiyojga qarab istalgan shaklda sarflash mumkin. Misol berilgan 8000 Unitlarni 8000 mb uchun yoki 8000 daqiqa so'zlashish uchun foydalanish mumkin yoki 4000 mb va 4000 daqiqa uchun.",
+        reminder:
+            "Units tariff rejalarida oylik internet paketlar xarid qilib bo'lmaydi va berilgan Unitlar tugagandan so'ng hisobdagi mablag' orqali so'zlashib bo'lmaydi tarif rejasi tarif rejasi bo'yicha abonent to'lovi to'lab restar (*555#) xizmatini faollashtirish mumkin!",
+        codes: "777*4#"),
+    Unit_packages(
+        package_name: "11000",
+        monthly: "Oyiga 45000",
+        limits: "11000",
+        contents:
+            "1 Unit = 1mb, 1daqiqa, 1sms Yani berilgan Unitlarni extiyojga qarab istalgan shaklda sarflash mumkin. Misol berilgan 11000 Unitlarni 11000 mb uchun yoki 11000 daqiqa so'zlashish uchun foydalanish mumkin yoki 5500 mb va 5500 daqiqa uchun.",
+        reminder:
+            "1 Unit = 1mb, 1daqiqa, 1sms Yani berilgan Unitlarni extiyojga qarab istalgan shaklda sarflash mumkin. Misol berilgan 11000 Unitlarni 11000 mb uchun yoki 11000 daqiqa so'zlashish uchun foydalanish mumkin yoki 5500 mb va 5500 daqiqa uchun.",
+        codes: "*777*5#"),
+    Unit_packages(
+        package_name: "14000",
+        monthly: "Oyiga 55000",
+        limits: "14000",
+        contents:
+            "1 Unit = 1mb, 1daqiqa, 1sms Yani berilgan Unitlarni extiyojga qarab istalgan shaklda sarflash mumkin. Misol berilgan 14000 Unitlarni 14000 mb uchun yoki 14000 daqiqa so'zlashish uchun foydalanish mumkin yoki 7000 mb va 7000 daqiqa uchun.",
+        reminder:
+            "1 Unit = 1mb, 1daqiqa, 1sms Yani berilgan Unitlarni extiyojga qarab istalgan shaklda sarflash mumkin. Misol berilgan 14000 Unitlarni 14000 mb uchun yoki 14000 daqiqa so'zlashish uchun foydalanish mumkin yoki 7000 mb va 7000 daqiqa uchun.",
+        codes: "*777*6#"),
+    Unit_packages(
+        package_name: "18000",
+        monthly: "Oyiga 65000",
+        limits: "18000",
+        contents:
+            "1 Unit = 1mb, 1daqiqa, 1sms Yani berilgan Unitlarni extiyojga qarab istalgan shaklda sarflash mumkin. Misol berilgan 14000 Unitlarni 14000 mb uchun yoki 14000 daqiqa so'zlashish uchun foydalanish mumkin yoki 7000 mb va 7000 daqiqa uchun.",
+        reminder:
+            "1 Unit = 1mb, 1daqiqa, 1sms Yani berilgan Unitlarni extiyojga qarab istalgan shaklda sarflash mumkin. Misol berilgan 14000 Unitlarni 14000 mb uchun yoki 14000 daqiqa so'zlashish uchun foydalanish mumkin yoki 7000 mb va 7000 daqiqa uchun.",
+        codes: "*777*7#"),
+    Unit_packages(
+        package_name: "22000",
+        monthly: "Oyiga 75000",
+        limits: "22000",
+        contents:
+            "1 Unit = 1mb, 1daqiqa, 1sms Yani berilgan Unitlarni extiyojga qarab istalgan shaklda sarflash mumkin. Misol berilgan 22000 Unitlarni 22000 mb uchun yoki 22000 daqiqa so'zlashish uchun foydalanish mumkin yoki 11000 mb va 11000 daqiqa uchun.",
+        reminder:
+            "1 Unit = 1mb, 1daqiqa, 1sms Yani berilgan Unitlarni extiyojga qarab istalgan shaklda sarflash mumkin. Misol berilgan 22000 Unitlarni 22000 mb uchun yoki 22000 daqiqa so'zlashish uchun foydalanish mumkin yoki 11000 mb va 11000 daqiqa uchun.",
+        codes: "*777*8#")
   ];
 
   @override
@@ -55,13 +109,15 @@ Widget Units_item(Unit_packages unit_packages) {
             padding: const EdgeInsets.all(4.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              // ignore: prefer_const_literals_to_create_immutables
               children: [
                 Icon(
                   Icons.save_alt_outlined,
                   color: Colors.blue,
                 ),
-                Text(unit_packages.package_name),
+                Text(
+                  "UNITS ${unit_packages.package_name}",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                ),
                 const Icon(
                   Icons.show_chart_outlined,
                   color: Colors.blue,
